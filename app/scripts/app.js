3fa04e683f66c9835dcca50fe7d99ea66d9bfea2
Garrett Sussman <gasussman@gmail.com>
2014-08-12 18:25:07 +0000

    Changed slider to update (music location) data on change.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,11 @@
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
      ]
  };
  
@@ -173,6 +173,14 @@ blocJams.controller('Song.controller', ['$scope', function($scope) {
        var song = this.currentAlbum.songs[currentTrackIndex];
        this.setSong(this.currentAlbum, song);
      },
+
+     seek: function(time) {
+       // Checks to make sure that a sound file is playing before seeking.
+       if(currentSoundFile) {
+         // Uses a Buzz method to set the time of the song.
+         currentSoundFile.setTime(time);
+       }
+     },
      
      setSong: function(album, song) {
       if (currentSoundFile) {
@@ -202,17 +210,44 @@ blocJams.controller('Song.controller', ['$scope', function($scope) {
      return offsetXPercent;
    }
 
+   var numberFromValue = function(value, defaultValue) {
+     if (typeof value === 'number') {
+       return value;
+     }
+ 
+     if(typeof value === 'undefined') {
+       return defaultValue;
+     }
+ 
+     if(typeof value === 'string') {
+       return Number(value);
+     }
+   }
+
    return {
      templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
      replace: true,
      restrict: 'E',
-     scope: {},
+     scope: {
+      onChange: '&'
+    },
      link: function(scope, element, attributes) {
        scope.value = 0;
-       scope.max = 200;
+       scope.max = 100;
        var $seekBar = $(element);
+       
+      attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+ 
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
+
        var percentString = function () {
-         percent = Number(scope.value) / Number(scope.max)  * 100;
+         var value = scope.value || 0;
+         var max = scope.max || 100;
+         percent = value / max * 100;
          return percent + "%";
        }
  
@@ -227,13 +262,16 @@ blocJams.controller('Song.controller', ['$scope', function($scope) {
        scope.onClickSlider = function(event) {
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.value = percent * scope.max;
+         notifyCallback(scope.value);
        }
 
        scope.trackThumb = function() {
          $document.bind('mousemove.thumb', function(event){
            var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
            scope.$apply(function(){
-             scope.value = percent * scope.max;
+           scope.value = percent * scope.max;
+           notifyCallback(scope.value);
+
            });
          });
  
@@ -242,6 +280,12 @@ blocJams.controller('Song.controller', ['$scope', function($scope) {
            $document.unbind('mousemove.thumb');
            $document.unbind('mouseup.thumb');
          });
+       }
+
+       var notifyCallback = function(newValue) {
+         if(typeof scope.onChange === 'function') {
+           scope.onChange({value: newValue});
+         }
        };
      }
    };
